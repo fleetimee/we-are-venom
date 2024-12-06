e659be4e541c1b7fbdc159949c860060f946d5c0
cecep.cahyana <cecep.cahyana@bpddiy.co.id>
2024-12-06 15:25:26 +0700

    refactor menubar component

--- components/MenuBar.tsx
+++ components/MenuBar.tsx
@@ -1,21 +1,19 @@
-// components/MenuBar.tsx
-
 import { useState, useEffect } from 'react';
+import DesktopNavLinks from './DesktopNavLinks';
+import MobileDrawer from './MobileDrawer';
+import MobileMenuButton from './MobileMenuButton';
 import Link from 'next/link';
-import HomeIcon from './HomeIcon'; // Import the HomeIcon component
+import HomeIcon from './HomeIcon';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 const MenuBar = () => {
     const [isScrolled, setIsScrolled] = useState(false);
+    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
     useEffect(() => {
         const handleScroll = () => {
-            if (window.scrollY > 50) {
-                setIsScrolled(true);
-            } else {
-                setIsScrolled(false);
-            }
+            setIsScrolled(window.scrollY > 50);
         };
         window.addEventListener('scroll', handleScroll);
         return () => {
@@ -23,84 +21,43 @@ const MenuBar = () => {
         };
     }, []);
 
+    const handleDrawerToggle = () => {
+        setIsDrawerOpen(!isDrawerOpen);
+    };
+
     return (
         <header
-                className={`fixed top-0 left-0 w-full transition-all duration-300 ${isScrolled ? 'bg-white py-2 shadow-xl' : 'bg-transparent py-4'} z-50`}
-            >
-                <nav className="container mx-auto flex justify-between items-center px-4">
-                    <div className={`pl-24 ${isScrolled ? 'text-darkBlue' : 'text-white'}`} style={{ width: '200px', height: 'auto' }}>
-                        <object
-                            type="image/svg+xml"
-                            data="/bpd.svg"
-                            style={{
-                                width: '100%',
-                                height: '100%',
-                                fill: isScrolled ? 'darkblue' : 'white',
-                                stroke: isScrolled ? 'darkblue' : 'white'
-                            }}>
-                            Your browser does not support SVG
-                        </object>
-                    </div>
-                    <ul className="flex pr-24">
-                        <li className="mr-4">
-                            <Link href="/" legacyBehavior>
-                                <a className={`flex items-center font-semibold py-2 px-2 block custom-underline hover:font-bold ${isScrolled ? 'text-darkBlue' : 'hover:text-blue-300'}`}>
-                                    <HomeIcon fill={isScrolled ? 'darkblue' : 'white'} />
-                                </a>
-                            </Link>
-                        </li>
-                        <li className="mr-4">
-                            <Link href="/karir" legacyBehavior>
-                                <a className={`font-semibold py-2 px-2 block custom-underline hover:font-bold ${isScrolled ? 'text-darkBlue' : 'hover:text-blue-300'}`}>
-                                    Karir
-                                </a>
-                            </Link>
-                        </li>
-                        <li className="mr-4">
-                            <Link href="/magang" legacyBehavior>
-                                <a className={`font-semibold py-2 px-2 block custom-underline hover:font-bold ${isScrolled ? 'text-darkBlue' : 'hover:text-blue-300'}`}>
-                                    Magang
-                                </a>
-                            </Link>
-                        </li>
-                        <li className="mr-4">
-                            <Link href="/riwayat" legacyBehavior>
-                                <a className={`font-semibold py-2 px-2 block custom-underline hover:font-bold ${isScrolled ? 'text-darkBlue' : 'hover:text-blue-300'}`}>
-                                    Riwayat
-                                </a>
-                            </Link>
-                        </li>
-                        <li className="mr-4">
-                            <Link href="/pengumuman" legacyBehavior>
-                                <a className={`font-semibold py-2 px-2 block custom-underline hover:font-bold ${isScrolled ? 'text-darkBlue' : 'hover:text-blue-300'}`}>
-                                    Pengumuman
-                                </a>
-                            </Link>
-                        </li>
-                        <li className="mr-4">
-                            <Link href="/info-artikel" legacyBehavior>
-                                <a className={`font-semibold py-2 px-2 block custom-underline hover:font-bold ${isScrolled ? 'text-darkBlue' : 'hover:text-blue-300'}`}>
-                                    Info & Artikel
-                                </a>
-                            </Link>
-                        </li>
-                        <li className="mr-4">
-                            <Link href="/login" legacyBehavior>
-                                <a className={`font-semibold py-2 px-2 block custom-underline hover:font-bold ${isScrolled ? 'text-darkBlue' : 'hover:text-blue-300'}`}>
-                                    Login
-                                </a>
-                            </Link>
-                        </li>
-                        <li className="mr-4">
-                            <Link href="/profil" legacyBehavior>
-                                <a className={`font-semibold py-2 px-2 block custom-underline hover:font-bold ${isScrolled ? 'text-darkBlue' : 'hover:text-blue-300'}`}>
-                                    Profil
-                                </a>
-                            </Link>
-                        </li>
-                    </ul>
-                </nav>
-            </header>
+            className={`fixed top-0 left-0 w-full transition-all duration-300 ${isScrolled ? 'bg-white py-2 shadow-xl' : 'bg-transparent py-4'} z-50`}
+        >
+            <nav className="container mx-auto flex justify-between items-center px-4">
+                {/* Logo Section */}
+                <div className={`pl-4 sm:pl-24 ${isScrolled ? 'text-darkBlue' : 'text-white'}`} style={{ width: '200px', height: 'auto' }}>
+                    <object
+                        type="image/svg+xml"
+                        data="/bpd.svg"
+                        style={{
+                            width: '100%',
+                            height: '100%',
+                            fill: isScrolled ? 'darkBlue' : 'white',
+                            stroke: isScrolled ? 'darkBlue' : 'white',
+                        }}
+                    >
+                        Your browser does not support SVG
+                    </object>
+                </div>
+
+                {/* Desktop Navigation */}
+                <div className="hidden sm:flex relative py-6 flex-col justify-center">
+                    <DesktopNavLinks />
+                </div>
+
+                {/* Mobile Navigation */}
+                <div className="sm:hidden relative flex flex-row my-4">
+                    <MobileMenuButton onClick={handleDrawerToggle} />
+                    <MobileDrawer isOpen={isDrawerOpen} onClose={handleDrawerToggle} />
+                </div>
+            </nav>
+        </header>
     );
 };
 
