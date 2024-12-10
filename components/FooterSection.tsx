406ba528561397605159e458dbe4c822793ae004
cecep.cahyana <cecep.cahyana@bpddiy.co.id>
2024-12-10 11:54:58 +0700

    improve footer responsive

--- components/FooterSection.tsx
+++ components/FooterSection.tsx
@@ -1,111 +1,68 @@
-// components/FooterSection.tsx
-
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faInstagram, faYoutube } from "@fortawesome/free-brands-svg-icons";
 
 const FooterSection = () => {
     const currentYear = new Date().getFullYear();
 
     return (
-        <>
-            <div className="flex relative z-10 pt-14 bg-white">
-                <div className="w-1/2 h-min-24 px-4 bg-white text-black flex flex-col relative z-0">
-                    <p
-                        className="text-xl font-bold text-darkBlue"
-                        style={{
-                            margin: '0',
-                            fontFamily: 'Open Sans, sans-serif',
-                            textAlign: 'left',
-                            backgroundColor: '#fff',
-                            marginLeft: '8rem',
-                        }}
-                    >
-                        Kantor Pusat
-                    </p>
-                    <p
-                        className="mt-4"
-                        style={{
-                            margin: '0',
-                            fontFamily: 'Open Sans, sans-serif',
-                            fontSize: '0.8rem',
-                            fontWeight: 400,
-                            lineHeight: 1.8,
-                            color: '#222',
-                            textAlign: 'left',
-                            backgroundColor: '#fff',
-                            marginRight: 18,
-                            marginLeft: '8rem',
-                        }}
-                    >
+        <div className="bg-white pt-14 px-6 md:px-12 lg:px-24 text-black">
+            {/* Responsive Grid */}
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                {/* Office Information */}
+                <div className="flex flex-col">
+                    <h2 className="text-xl font-bold text-darkBlue mb-4">Kantor Pusat</h2>
+                    <p className="text-sm text-gray-700 leading-relaxed">
                         Jl. Tentara Pelajar No.7, Bumijo, Kec. Jetis, Kota Yogyakarta, Daerah Istimewa Yogyakarta 55231
-                        <br/>
-                        <br/>
-                        PHONE : +62274561614<br/>
-                        EMAIL : contactcenter@bpddiy.co.id<br/>
-                        FAX : +62274561614<br/>
-                        <br/>
+                        <br />
+                        <br />
+                        PHONE : +62274561614
+                        <br />
+                        EMAIL : contactcenter@bpddiy.co.id
+                        <br />
+                        FAX : +62274561614
+                        <br />
                     </p>
                 </div>
-                <div className="w-1/4 px-4 bg-white text-black flex flex-col relative z-0">
-                    <p
-                        className="text-xl font-bold text-darkBlue"
-                        style={{
-                            margin: '0',
-                            fontFamily: 'Open Sans, sans-serif',
-                            textAlign: 'left',
-                            backgroundColor: '#fff',
-                            marginLeft: '8rem',
-                            marginBottom: '1rem',
-                        }}
-                    >
-                        Hubungi Kami
-                    </p>
-                    <ul
-                        className="mt-4"
-                        style={{
-                            margin: '0',
-                            fontFamily: 'Open Sans, sans-serif',
-                            fontSize: '0.8rem',
-                            fontWeight: 400,
-                            lineHeight: 1.8,
-                            color: '#222',
-                            textAlign: 'left',
-                            backgroundColor: '#fff',
-                            marginRight: 18,
-                            marginLeft: '8rem',
-                            listStyleType: 'none',
-                            padding: 0
-                        }}
-                    >
-                        <li style={{ marginBottom: '1rem' }}>
-                            <button onClick={() => window.open('https://facebook.com', '_blank')}
-                                    className="flex items-center hover:text-darkBlue">
-                                <FontAwesomeIcon icon={faFacebook} style={{ marginRight: '8px' }}/>
+
+                {/* Contact Us */}
+                <div className="flex flex-col">
+                    <h2 className="text-xl font-bold text-darkBlue mb-4">Hubungi Kami</h2>
+                    <ul className="text-sm text-gray-700 space-y-4">
+                        <li>
+                            <button
+                                onClick={() => window.open("https://facebook.com", "_blank")}
+                                className="flex items-center hover:text-darkBlue"
+                            >
+                                <FontAwesomeIcon icon={faFacebook} className="mr-2" />
                                 FACEBOOK
                             </button>
                         </li>
-                        <li style={{ marginBottom: '1rem' }}>
-                            <button onClick={() => window.open('https://instagram.com', '_blank')}
-                                    className="flex items-center hover:text-purple-700">
-                                <FontAwesomeIcon icon={faInstagram} style={{ marginRight: '8px' }}/>
+                        <li>
+                            <button
+                                onClick={() => window.open("https://instagram.com", "_blank")}
+                                className="flex items-center hover:text-purple-700"
+                            >
+                                <FontAwesomeIcon icon={faInstagram} className="mr-2" />
                                 INSTAGRAM
                             </button>
                         </li>
-                        <li style={{ marginBottom: '1rem' }}>
-                            <button onClick={() => window.open('https://youtube.com', '_blank')}
-                                    className="flex items-center hover:text-red-700">
-                                <FontAwesomeIcon icon={faYoutube} style={{ marginRight: '8px' }}/>
+                        <li>
+                            <button
+                                onClick={() => window.open("https://youtube.com", "_blank")}
+                                className="flex items-center hover:text-red-700"
+                            >
+                                <FontAwesomeIcon icon={faYoutube} className="mr-2" />
                                 YOUTUBE
                             </button>
                         </li>
                     </ul>
                 </div>
-                <div className="w-1/4 px-4 bg-white text-black flex relative z-0">
 
-                </div>
+                {/* Placeholder for Future Content */}
+                <div className="flex flex-col"></div>
             </div>
-        </>
+        </div>
     );
 };
 
