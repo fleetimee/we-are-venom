2962731a6cab5a117f5f8e28a2e7d3cb8f1e1523
cecep.cahyana <cecep.cahyana@bpddiy.co.id>
2024-12-10 11:54:00 +0700

    integrate with login API

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,117 +1,188 @@
 "use client";
 
 import React, { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { toast } from "sonner";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import MenuBar from "../../../components/MenuBar";
-import FooterSection from "../../../components/FooterSection";
 import FooterCopyright from "../../../components/FooterCopyright";
 import { ScrollToTopButton } from "../../../components/ScrollToTopButton";
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import DekstopNavLinksAlt from '../../../components/DekstopNavLinksAlt';
+import MobileDrawer from '../../../components/MobileDrawer';
+import MobileMenuButton from '../../../components/MobileMenuButton';
+
+// Define the form schema using zod
+const formSchema = z.object({
+  email: z.string().email("Please enter a valid email."),
+  password: z.string().min(6, "Password must be at least 6 characters."),
+});
+
+type LoginFormValues = z.infer<typeof formSchema>;
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [showPassword, setShowPassword] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-  const handleLogin = (e: React.FormEvent) => {
-    e.preventDefault();
-    console.log("Login submitted:", { email, password });
+  // Initialize the form with react-hook-form and zod resolver
+  const form = useForm<LoginFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  // Handle form submission
+  const handleLogin = async (data: LoginFormValues) => {
+    setLoading(true);
+    try {
+      const response = await fetch("http://localhost:8080/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (!response.ok) {
+        const errorText = await response.text();
+        throw new Error(`API error: ${response.status} - ${errorText}`);
+      }
+
+      const result = await response.json();
+
+      if (result.responseCode === "000") {
+        // Login successful
+        localStorage.setItem("token", result.data.token);
+
+        // Show success toast
+        toast.success("Login successful! Redirecting...", {
+          style: {
+            backgroundColor: "white", // White background
+            color: "#4CAF50", // Green text color
+            borderRadius: "8px", // Rounded corners
+            padding: "10px 20px", // Padding
+          },
+        });
+
+        // Redirect to another page after a short delay
+        setTimeout(() => {
+          window.location.href = "/karir";
+        }, 2000);
+      } else {
+        // Show error toast for server validation error
+        toast.error(result.responseMessage || "Login failed. Please try again.");
+      }
+    } catch (error) {
+      console.error("Error during login:", error);
+      toast.error("An error occurred. Please try again later.");
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
     <div className="min-h-screen bg-white font-sans relative">
-      {/* Section MenuBar */}
-      <MenuBar />
+      <nav className="container mx-auto flex justify-between items-center px-4">
+        {/* Logo Section */}
+            <div
+                    className={`pl-4 sm:pl-24 text-darkBlue`}
+                    style={{ width: '300px', height: 'auto' }}
+                    >
+                    <Image
+                        src={"/images/Logo_Color.png"} // Change the logo based on scroll state
+                        alt="BPD Logo"
+                        width={200}
+                        height={0} // Auto-scale height
+                        priority
+                    />
+            </div>
+
+        {/* Desktop Navigation */}
+        <div className="hidden sm:flex relative py-6 flex-col justify-center">
+          <DekstopNavLinksAlt />
+        </div>
+      </nav>
+      
 
       <main className="flex items-center justify-center pt-20 min-h-screen">
         <div className="bg-white rounded-lg shadow-lg p-8 w-full max-w-md">
           <h2 className="text-2xl font-bold text-center text-darkBlue mb-6">
             Login to Your Account
           </h2>
-          <form onSubmit={handleLogin}>
-            {/* Email Input */}
-            <div className="mb-4">
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Email Address
-              </label>
-              <input
-                type="email"
-                id="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-                className="mt-1 w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 text-gray-700"
-                placeholder="Enter your email"
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(handleLogin)} className="space-y-6">
+              {/* Email Field */}
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email Address</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Enter your email"
+                        type="email"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
               />
+
+              {/* Password Field */}
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Enter your password"
+                        type="password"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              {/* Submit Button */}
+              <Button
+                type="submit"
+                className="w-full bg-darkBlue text-white font-semibold py-2 rounded-md hover:bg-blue-700 transition"
+                disabled={loading}
+              >
+                {loading ? "Logging in..." : "Login"}
+              </Button>
+            </form>
+          </Form>
+
+          <div className="text-center text-gray-700 mt-4">
+                Do not have account?{" "}
+                <a href="/register" className="text-blue-500 hover:underline">
+                Register
+                </a>
             </div>
-
-            {/* Password Input */}
-            <div className="mb-6">
-                <label
-                    htmlFor="password"
-                    className="block text-sm font-medium text-gray-700"
-                >
-                    Password
-                </label>
-                <div className="relative">
-                    <input
-                    type={showPassword ? "text" : "password"}
-                    id="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                    className="mt-1 w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 text-gray-700 pr-10"
-                    placeholder="Enter your password"
-                    />
-                    <button
-                    type="button"
-                    onClick={() => setShowPassword((prev) => !prev)}
-                    className="absolute inset-y-0 right-3 flex items-center text-sm text-gray-500 hover:text-blue-500"
-                    >
-                    {showPassword ? "Hide" : "Show"}
-                    </button>
-                </div>
-                </div>
-
-
-            {/* Submit Button */}
-            <button
-              type="submit"
-              className="w-full bg-darkBlue text-white font-semibold py-2 rounded-md hover:bg-blue-700 transition"
-            >
-              Login
-            </button>
-          </form>
-
-          {/* Links */}
-          <div className="mt-4 text-center text-sm text-gray-600">
-            <p>
-              Don't have an account?{" "}
-              <a
-                href="/signup"
-                className="text-blue-600 hover:underline transition"
-              >
-                Sign up
-              </a>
-            </p>
-            <p className="mt-2">
-              Forgot your password?{" "}
-              <a
-                href="/forgot-password"
-                className="text-blue-600 hover:underline transition"
-              >
-                Reset it here
-              </a>
-            </p>
-          </div>
         </div>
       </main>
 
       {/* Section Footer */}
-      {/* <FooterSection /> */}
-
-      {/* Section Copyright */}
       <FooterCopyright />
 
       {/* Scroll to Top Button */}
